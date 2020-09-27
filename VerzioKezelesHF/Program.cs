07c6a5b513b642804c858eebb8682007349aed0c
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-09-27 22:37:49 +0200

    1 feladat kész.

--- VerzioKezelesHF/Program.cs
+++ VerzioKezelesHF/Program.cs
@@ -10,6 +10,22 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+            int[] szamok = new int[10];
+            Console.WriteLine("Kérem adjon meg 10 számot:");
+            
+            for (int i = 0; i < szamok.Length; i++)
+            {
+
+             int h = Convert.ToInt32(Console.ReadLine());
+                szamok[i] = h;
+            }
+            int atlag = 0;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                atlag = atlag + szamok[i];
+            }
+            Console.WriteLine("A számok átlaga : " + atlag/szamok.Length);
+            Console.ReadKey();
         }
     }
 }
